cb701dda9f99e3db7061057e51c3953051cdfd47
Sena <sena250817@gmail.com>
2026-08-09 23:19:33 +0300

    Revert schema_extra and add target back based on NovaVision specification

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -61,7 +61,7 @@ class ConfigTriggeringAnchor(Config):
     field: Literal["dropdownlist"] = "dropdownlist"
     class Config:
         title = "Tracking Anchor"
-        json_schema_extra = {"target": "value"}
+        schema_extra = {"target": "value"}
 
 
 class ConfigReferencePath(Config):
@@ -100,7 +100,7 @@ class PathDeviationRequest(Request):
     inputs: Optional[PathDeviationInputs]
     configs: PathDeviationConfigs
     class Config:
-        json_schema_extra = {"target": "configs"}
+        schema_extra = {"target": "configs"}
 
 
 class PathDeviationResponse(Response):
@@ -114,7 +114,7 @@ class PathDeviationTrackerExecutorConfig(Config):
     field: Literal["option"] = "option"
     class Config:
         title = "Path Deviation"
-        json_schema_extra = {"target": {"value": 0}}
+        schema_extra = {"target": {"value": 0}}
 
 
 class ConfigExecutor(Config):
@@ -124,6 +124,7 @@ class ConfigExecutor(Config):
     field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
     class Config:
         title = "Task"
+        schema_extra = {"target": "value"}
 
 
 class PackageConfigs(Configs):
