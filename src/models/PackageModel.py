ca88eeced0665839970ae4147049c3299905b78a
Sena <sena250817@gmail.com>
2026-08-10 00:55:02 +0300

    Revert "Remove DummyExecutorConfig and add json_schema_extra target to ConfigExecutor as per single-executor specification"
    This reverts commit bf97ce32b9b2a7b3ecca9fc777c9eb261c89062a.

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -140,14 +140,22 @@ class PathDeviationTrackerExecutorConfig(Config):
         json_schema_extra = {"target": {"value": 0}}
 
 
+class DummyExecutorConfig(Config):
+    name: Literal["DummyExecutorConfig"] = "DummyExecutorConfig"
+    value: Literal["Dummy"] = "Dummy"
+    type: Literal["object"] = "object"
+    field: Literal["option"] = "option"
+    class Config:
+        title = "Seçmeyin"
+
+
 class ConfigExecutor(Config):
     name: Literal["ConfigExecutor"] = "ConfigExecutor"
-    value: Union[PathDeviationTrackerExecutorConfig]
+    value: Union[PathDeviationTrackerExecutorConfig, DummyExecutorConfig]
     type: Literal["executor"] = "executor"
     field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
     class Config:
         title = "Task"
-        json_schema_extra = {"target": "value"}
 
 
 class PackageConfigs(Configs):
