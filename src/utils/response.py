f7a6bf1793b3f1991ef4e8de404ddf6569a3e126
Sena <sena250817@gmail.com>
2026-08-10 10:04:12 +0300

    Fix import paths to use novavision.path_deviation_tracker namespace instead of src

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,6 +1,6 @@
 from sdks.novavision.src.helper.package import PackageHelper
 
-from src.models.PackageModel import (
+from novavision.path_deviation_tracker.models.PackageModel import (
     ConfigExecutor, OutputDetections, PackageConfigs, PackageModel,
     PathDeviationTrackerExecutorConfig, PathDeviationOutputs, PathDeviationResponse,
 )
